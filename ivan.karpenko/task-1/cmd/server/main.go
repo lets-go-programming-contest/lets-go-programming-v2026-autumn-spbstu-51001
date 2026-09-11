24ab9417fb2e90e9911e82d4843c68997f8b1d9e
restorte <vanyakarpenko837@yandex.ru>
2026-09-11 12:21:00 +0300

    [TASK-1] implementation calc

--- ivan.karpenko/task-1/cmd/server/main.go
+++ ivan.karpenko/task-1/cmd/server/main.go
@@ -1,5 +1,52 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var num1 int
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var num2 int
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var op string
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	var res int
+	switch op {
+	case "+":
+		res = num1 + num2
+		return
+	case "-":
+		res = num1 - num2
+		return
+	case "*":
+		res = num1 * num2
+		return
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		res = num1 / num2
+		return
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+	fmt.Println(res)
 
 }
