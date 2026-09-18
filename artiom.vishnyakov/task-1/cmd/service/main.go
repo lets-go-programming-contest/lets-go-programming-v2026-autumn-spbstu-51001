be3288155ba1fb96ea5a03fb3f54a57a6e1638f8
ArtConion <artiom0408@mail.ru>
2026-09-18 16:55:51 +0300

    [TASK-1] Task complete

--- artiom.vishnyakov/task-1/cmd/service/main.go
+++ artiom.vishnyakov/task-1/cmd/service/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	var oper string
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err := fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err := fmt.Scan(&oper)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch oper {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
 }
