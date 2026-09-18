be8089898879cc85da2de8d39b1e716da686a473
swagainator <platonalekseevi6@gmail.com>
2026-09-18 12:33:37 +0300

    [TASK-1] Add operand input validation

--- platon.alekseev/task-1/cmd/service/main.go
+++ platon.alekseev/task-1/cmd/service/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Calculator")
+	var firstOperand int
+	_, err := fmt.Scan(&firstOperand)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var secondOperand int
+	_, err = fmt.Scan(&secondOperand)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var operation string
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
