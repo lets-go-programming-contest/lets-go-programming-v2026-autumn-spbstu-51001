64c9568cfea1e48c7a06409b10402f1348abe304
CWSHBR <mrcookie209@gmail.com>
2026-09-22 15:17:00 +0300

    [TASK-1] Implement calculator operations

--- bulat.shaykhraziev/task-1/cmd/service/main.go
+++ bulat.shaykhraziev/task-1/cmd/service/main.go
@@ -29,4 +29,23 @@ func main() {
 
 		return
 	}
+
+	switch operation {
+	case "+":
+		fmt.Println(first + second)
+	case "-":
+		fmt.Println(first - second)
+	case "*":
+		fmt.Println(first * second)
+	case "/":
+		if second == 0 {
+			fmt.Println("Division by zero")
+
+			return
+		}
+
+		fmt.Println(first / second)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
